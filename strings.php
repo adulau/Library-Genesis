8a9785e1b6423afbf9dee27d9e0d3b9620e790c1
bookwarrior <bookwarrior@81c3e747-05a8-4331-b00c-47dff4a7b488>
2010-06-12 02:42:13 +0000

    added message posting on the front page, see strings.php, $message
    git-svn-id: http://svn2.assembla.com/svn/libgen@41 81c3e747-05a8-4331-b00c-47dff4a7b488

--- strings.php
+++ strings.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 	$searchtip = "<p>Enter exact string or its fragment, e.g. <i><u>analy</u></i> for all entries like <i><u>analy</u></i>sis, <i><u>analy</u></i>tical etc.<br> 
 Search in column Title, Author, Publisher, Series, Periodicsl, MD5</p>";
 	$str_prev = "&lt; &lt; &lt; PREV";
@@ -8,4 +8,5 @@ Search in column Title, Author, Publisher, Series, Periodicsl, MD5</p>";
 	$str_edition_ru = "изд.";
 	$str_edition_en = "ed.";
 	$str_keywords = "eBooks,books,electronic library,library,science,tech,sci-tech,scientific literature,pdf,djvu,physics,medicine,biology,chemistry,geology,math,mathematics,engineering,computer,electrical engineering,электронные книги,электронная библиотека,KoLXo3,колхоз,библиотека колхоз,мехмат,mexmat,lmr,lib.mexmat.ru,homelab,хомлаб,спецхран,спец. хран.,Great Science Textbooks,техника,компьютеры,электротехника,электроника,физика,математика,химия,биология,геология,литература,электронная литература,open source,free ebooks,free books,ebook community,librarians";
+	$message = "";
 ?>
